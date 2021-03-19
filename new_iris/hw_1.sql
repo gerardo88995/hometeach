91d23819616cc5b7f7f89ecc51f85eda68509c0d
gerardo88995 <jerry020435@gmail.com>
2021-03-19 14:43:17 +0800

    deleting the garbled code

--- new_iris/hw_1.sql
+++ new_iris/hw_1.sql
@@ -1,5 +1,4 @@
 
-
 SELECT avg(sepalwidth)
    FROM  iris_1
    GROUP BY class;
--- new_iris/hw_4.sql
+++ new_iris/hw_4.sql
@@ -1,11 +1,3 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2578
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab840
-\pard\pardeftab840\ri-340\qj\partightenfactor0
-
-\f0\fs21 \cf0 SELECT avg(petalwidth)\
-   FROM  iris_1\
-   GROUP BY class;}
+ SELECT avg(petalwidth)
+   FROM  iris_1
+   GROUP BY class;
