f9bdeb3ce990f97a51380f0901aa7c26bbe04231
gerardo88995 <jerry020435@gmail.com>
2021-04-03 00:13:11 +0800

    uploading sql

---
+++ new_iris/avg_feature.sql
@@ -0,0 +1,7 @@
+select class,
+           avg(Sepallength) as avg_sepal_length,
+		   avg(Sepalwidth) as avg_sepal_width,
+		   avg(petallength) as avg_petal_length,
+		   avg(petalwidth) as avg_petal_width
+from iris_1
+group by class;
