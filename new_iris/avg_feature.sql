ddf1d2f6dd59fce008b596a06dc4ccb92bf7b658
gerardo88995 <jerry020435@gmail.com>
2021-04-10 18:20:55 +0800

    adjusting indent

--- new_iris/avg_feature.sql
+++ new_iris/avg_feature.sql
@@ -1,7 +1,8 @@
-select class,
-           avg(Sepallength) as avg_sepal_length,
-		   avg(Sepalwidth) as avg_sepal_width,
-		   avg(petallength) as avg_petal_length,
-		   avg(petalwidth) as avg_petal_width
+select
+	class,
+    avg(Sepallength) as avg_sepal_length,
+	avg(Sepalwidth) as avg_sepal_width,
+	avg(petallength) as avg_petal_length,
+	avg(petalwidth) as avg_petal_width
 from iris_1
 group by class;
--- new_iris/upper_lower_bound.sql
+++ new_iris/upper_lower_bound.sql
@@ -1,7 +1,8 @@
-select class,
-           max(petallength) as max_petal_length,
-		   min(petallength) as min_petal_length,
-		   max(petalwidth) as max_petal_width,
-		   min(petalwidth) as min_petal_width
+select 
+	class,
+    max(petallength) as max_petal_length,
+	min(petallength) as min_petal_length,
+	max(petalwidth) as max_petal_width,
+	min(petalwidth) as min_petal_width
 from iris_1
 group by class;
