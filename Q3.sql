3165880e893f2438399d842ea8f07516477d64d0
gerardo88995 <jerry020435@gmail.com>
2021-06-02 11:38:35 +0800

    modifying q3.sql

--- Q3.sql
+++ Q3.sql
@@ -4,11 +4,21 @@ with t1 as(
 		pm25,
 		max(pm25) over (rows UNBOUNDED PRECEDING) as biggest
 	from preprocess
+),
+
+t2 as(
+	select 
+		dt,
+		biggest,
+		count(biggest) as num_biggest
+	from t1
+	group by biggest
 )
 
-select 
+SELECT
 	dt,
-	biggest,
-	count(biggest) as num_biggest
-from t1
-group by biggest
+	num_biggest,
+	biggest
+from t2
+order by num_biggest DESC
+limit 1
